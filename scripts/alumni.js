ca0feae068ae6b376a24d04db1d9a5c712453f89
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-12-08 17:39:55 +0000

    Add JS alumni card generation
    Move photos to assets
    Add role to json
    Remove hardcoded alumni cards
    Create specific css for each alumni page, which imports main and allows for custom styles

---
+++ scripts/alumni.js
@@ -0,0 +1,33 @@
+function slugify(text) {
+    return text
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, "-")
+        .replace(/[^\w-]+/g, "");
+}
+
+async function buildAlumniList() {
+    const data = await fetch("/data/profile_card_data.json").then((res) =>
+        res.json()
+    );
+    const alumniData = data.tfsAlum122025;
+    const alumniListContainer = document.querySelector("#alumni-list__cards");
+    const cardTemplate = document.querySelector("#card");
+
+    alumniData.forEach((alumnus) => {
+        const cardClone = cardTemplate.content.cloneNode(true);
+        const nameSlug = slugify(alumnus.name);
+
+        cardClone.querySelector(".card").classList.add(`card--${nameSlug}`);
+        cardClone.querySelector(".card__photo").src = alumnus.photo;
+        cardClone.querySelector(".card__name").textContent = alumnus.name;
+        cardClone.querySelector(".card__role").textContent = alumnus.role;
+        cardClone.querySelector(".card__link").href = alumnus.portfolio;
+        cardClone.querySelector(".card__description").textContent =
+            alumnus.description;
+
+        alumniListContainer.appendChild(cardClone);
+    });
+}
+
+buildAlumniList();
